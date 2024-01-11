4410ecddf671c546593f1fa5236d7f059e1bf57f
ANDR1JA <114744611+LuzzanD@users.noreply.github.com>
2024-01-11 22:32:46 +0100

    project added

--- Components/About.jsx
+++ Components/About.jsx
@@ -1,31 +1,41 @@
-import andrija from "../public/andrija.png"
-import Image from 'next/image'
+import andrija from "../public/andrija.png";
+import Image from "next/image";
 
 const About = () => {
   return (
-   <div id="about" className="-mb-20 md:-mb-8 xl:-mb-10 pt-2">
-     <h2 className="mb-12 sm:mb-16 mt-10 xl:mt-16 text-3xl md:text-4xl xl:text-5xl tracking-widest dark:text-zinc-100">About</h2>
-     <div className="flex flex-col lg:flex-row-reverse relative justify-end lg:justify-between gap-12">
+    <div id="about" className="-mb-20 md:-mb-8 xl:-mb-10 pt-2">
+      <h2 className="mb-12 sm:mb-16 mt-10 xl:mt-16 text-3xl md:text-4xl xl:text-5xl tracking-widest dark:text-zinc-100">
+        About
+      </h2>
+      <div className="flex flex-col lg:flex-row-reverse relative justify-end lg:justify-between gap-12">
         <div className="rounded-full h-52 w-52 sm:h-64 sm:w-64 xl:w-80 xl:h-80 relative right-0 lg:top-8 overflow-hidden border-[5px]">
-          <Image src={andrija} layout="fill" objectFit="cover"/>
+          <Image
+            src={andrija}
+            layout="fill"
+            objectFit="cover"
+            alt="profile picture"
+          />
         </div>
         <div className="flex flex-col items-start w-full lg:w-[65%] justify-start gap-6">
           <h2 className="mt-0 sm:mt-4 lg:mt-8 uppercase text-3xl sm:text-4xl lg:text-5xl drop-shadow-grad text-transparent bg-clip-text bg-gradient-to-r from-zinc-600 to-zinc-300">
-              Andrija Stojanović
+            Andrija Stojanović
           </h2>
           <h3 className="-mt-4 sm:-mt-2 mb-4 xl:mb-8 text-base md:text-lg xl:text-xl dark:text-zinc-100">
-              Junior Frontend / React Developer
+            Frontend Developer / Next Developer
           </h3>
           <p className="text-sm md:text-base xl:text-lg py-2 w-[85%] leading-6 md:leading-7 lg:leading-8 xl:leading-8 tracking-wider text-zinc-800 dark:text-zinc-100">
-            My name is Andrija Stojanovic, and I am a Civil Engineer by education who have decided to switch career path to web development and 
-            have been working mostly with React as a frontend JavaScript library. I am a highly motivated and analytical individual, 
-            with a strong interest in exploring various programming languages and opportunities within the IT industry. I am excited to take on 
-            new challenges and learn from experienced professionals in the field. 
-          </p>  
+            My name is Andrija Stojanovic, and I am a Civil Engineer by
+            education who has decided to switch career path to web development
+            and have been working mostly with React as a frontend JavaScript
+            library. I am a highly motivated and analytical individual, with a
+            strong interest in exploring various programming languages and
+            opportunities within the IT industry. I am excited to take on new
+            challenges and learn from experienced professionals in the field.
+          </p>
         </div>
-     </div>
-   </div>
-  )
-}
+      </div>
+    </div>
+  );
+};
 
-export default About
+export default About;
--- data/data.js
+++ data/data.js
@@ -1,40 +1,123 @@
-import ecomm from "../public/ecomm.png"
-import quiz from "../public/quizical.png"
-import scheme from "../public/scheme.png"
-import cart from "../public/cart.png"
-import port from "../public/portf.png"
-import spotify from "../public/Spotify.png"
-import { HtmlIcon, CssIcon, JavaScriptIcon, TypeScriptIcon, ReactIcon, TailwindIcon, NextIcon, SanityIcon, RapidApiIcon, ReduxIcon, AiFillGithubIcon, AiFillPhoneIcon, AiFillLinkedinIcon, HiMailIcon } from '../Utils/icons'
+/* eslint-disable react/jsx-key */
+import ecomm from "../public/ecomm.png";
+import quiz from "../public/quizical.png";
+import scheme from "../public/scheme.png";
+import cart from "../public/cart.png";
+import port from "../public/portf.png";
+import spotify from "../public/Spotify.png";
+import {
+  HtmlIcon,
+  CssIcon,
+  JavaScriptIcon,
+  TypeScriptIcon,
+  ReactIcon,
+  TailwindIcon,
+  NextIcon,
+  SanityIcon,
+  RapidApiIcon,
+  ReduxIcon,
+  AiFillGithubIcon,
+  AiFillPhoneIcon,
+  AiFillLinkedinIcon,
+  HiMailIcon,
+  NextJsIcon,
+  MongoDbIcon,
+} from "../Utils/icons";
 
 export const navbarData = [
-    {href: '/', section: 'About'}, 
-    {href: '#technologies', section: 'Technologies'}, 
-    {href: '#projects', section: 'Projects'}, 
-    {href: '#contact', section: 'Contact'}
-]
+  { href: "/", section: "About" },
+  { href: "#technologies", section: "Technologies" },
+  { href: "#projects", section: "Projects" },
+  { href: "#contact", section: "Contact" },
+];
 
 export const projectsData = [
-    {name: 'Spotify Clone', image: spotify, techs:[<TypeScriptIcon />, <TailwindIcon />, <ReactIcon />, <ReduxIcon/>], site: 'https://spotifyclone-kappa.vercel.app/', github:'https://github.com/LuzzanD/spotifyclone'},
-    {name: 'E-commerce', image: ecomm, techs:[ <CssIcon />, <SanityIcon />, <ReactIcon />, <NextIcon />], site: 'https://ecommerce-project-tawny.vercel.app', github:'https://github.com/LuzzanD/ecommerce-project'},
-    {name: 'Portfolio', image: port, techs:[<TailwindIcon />, <ReactIcon />, <NextIcon />], site: '', github:'https://github.com/LuzzanD/Portfolio'},
-    {name: 'Quiz', image: quiz, techs:[<HtmlIcon />, <CssIcon />, <ReactIcon />], site: 'https://andrijasquizical.netlify.app', github:'https://github.com/LuzzanD/Quiz'},
-    {name: 'Colour Scheme', image: scheme, techs:[<HtmlIcon />, <CssIcon />, <JavaScriptIcon />], site: 'https://andrijascolorscheme.netlify.app', github:'https://github.com/LuzzanD/Color-scheme-generator'},
-    {name: 'Shopping Cart', image: cart, techs:[<HtmlIcon />, <CssIcon />, <JavaScriptIcon />], site: 'https://restaurantordering.netlify.app', github:'https://github.com/LuzzanD/Restaurant-Ordering'},
-]
+  {
+    name: "StackOverflow Clone",
+    image: "",
+    techs: [
+      <NextJsIcon />,
+      <TypeScriptIcon />,
+      <TailwindIcon />,
+      <MongoDbIcon />,
+    ],
+    site: "https://overflow-five.vercel.app/",
+    github: "https://github.com/LuzzanD/overflow",
+  },
+  {
+    name: "Spotify Clone",
+    image: spotify,
+    techs: [<TypeScriptIcon />, <TailwindIcon />, <ReactIcon />, <ReduxIcon />],
+    site: "https://spotifyclone-kappa.vercel.app/",
+    github: "https://github.com/LuzzanD/spotifyclone",
+  },
+  {
+    name: "E-commerce",
+    image: ecomm,
+    techs: [<CssIcon />, <SanityIcon />, <ReactIcon />, <NextIcon />],
+    site: "https://ecommerce-project-tawny.vercel.app",
+    github: "https://github.com/LuzzanD/ecommerce-project",
+  },
+  {
+    name: "Portfolio",
+    image: port,
+    techs: [<TailwindIcon />, <ReactIcon />, <NextIcon />],
+    site: "",
+    github: "https://github.com/LuzzanD/Portfolio",
+  },
+  {
+    name: "Quiz",
+    image: quiz,
+    techs: [<HtmlIcon />, <CssIcon />, <ReactIcon />],
+    site: "https://andrijasquizical.netlify.app",
+    github: "https://github.com/LuzzanD/Quiz",
+  },
+  {
+    name: "Colour Scheme",
+    image: scheme,
+    techs: [<HtmlIcon />, <CssIcon />, <JavaScriptIcon />],
+    site: "https://andrijascolorscheme.netlify.app",
+    github: "https://github.com/LuzzanD/Color-scheme-generator",
+  },
+  //   {
+  //     name: "Shopping Cart",
+  //     image: cart,
+  //     techs: [<HtmlIcon />, <CssIcon />, <JavaScriptIcon />],
+  //     site: "https://restaurantordering.netlify.app",
+  //     github: "https://github.com/LuzzanD/Restaurant-Ordering",
+  //   },
+];
 
 export const technologiesData = [
-    <HtmlIcon />, 
-    <CssIcon />, 
-    <JavaScriptIcon />,
-    <TypeScriptIcon />,
-    <ReactIcon />, 
-    <TailwindIcon />, 
-    <NextIcon />
-]
+  <NextJsIcon />,
+  <ReactIcon />,
+  <TailwindIcon />,
+  <MongoDbIcon />,
+  <TypeScriptIcon />,
+  <JavaScriptIcon />,
+  <CssIcon />,
+  <HtmlIcon />,
+];
 
 export const contactData = [
-    {link: 'www.github.com/LuzzanD', href:'https://www.github.com/LuzzanD', icon: <AiFillGithubIcon/>},
-    {link: 'andrija0stojanovic@gmail.com', href:'mailto:andrija0stojanovic@gmail.com', icon: <HiMailIcon/>},
-    {link: 'www.linkedin.com/in/andrija-stojanović-1512411a6', href:'https://www.linkedin.com/in/andrija-stojanović-1512411a6', icon: <AiFillLinkedinIcon/>},
-    {link: '+381645696513', href:'tel:+381645696513', icon: <AiFillPhoneIcon/>},
-]
+  {
+    link: "www.github.com/LuzzanD",
+    href: "https://www.github.com/LuzzanD",
+    icon: <AiFillGithubIcon />,
+  },
+  {
+    link: "andrija0stojanovic@gmail.com",
+    href: "mailto:andrija0stojanovic@gmail.com",
+    icon: <HiMailIcon />,
+  },
+  {
+    link: "www.linkedin.com/in/andrija-stojanović-1512411a6",
+    href: "https://www.linkedin.com/in/andrija-stojanović-1512411a6",
+    icon: <AiFillLinkedinIcon />,
+  },
+  {
+    link: "+381645696513",
+    href: "tel:+381645696513",
+    icon: <AiFillPhoneIcon />,
+  },
+];
